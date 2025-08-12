e11bd4c25b7e8f4dd688571c4e67dba317225e02
sudarshan710 <sudarshanhope710@gmail.com>
2025-08-12 15:37:36 +0530

    sfd

--- dq.py
+++ dq.py
@@ -1,14 +1,26 @@
+import os
+os.environ['SPARK_VERSION'] = '3.3'
+
 from pydeequ.checks import Check, CheckLevel
 from pydeequ.verification import VerificationSuite
 from pyspark.sql import SparkSession
 
-spark = SparkSession.builder.appName("DataQualityChecks").getOrCreate()
+spark = (
+    SparkSession.builder
+    .appName("DataQualityChecks")
+    .config("spark.jars", "jars/deequ-2.0.3-spark-3.3.jar", "jars/scala-library-2.12.15.jar")  
+    .getOrCreate()
+)
+spark.sparkContext.setLogLevel("WARN")
+
 data = spark.read.csv("customers-100.csv", header=True, inferSchema=True)
-check = Check(spark, CheckLevel.Error, "Data quality checks").hasSize(lambda x: x >= 10000).isComplete("transaction_id").isNonNegative("sales_amount")
+check = Check(spark, CheckLevel.Error, "Data quality checks").hasSize(lambda x: x >= 100)
 
 result = VerificationSuite(spark).onData(data).addCheck(check).run()
 
 if result.status != "Success":
     print("Data quality checks failed. Blocking deployment.")
 
-exit(1)
+if result.status != "Success":
+    print("Data quality checks failed. Blocking deployment.")
+    exit(1)
